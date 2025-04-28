1973770ea07413d9907103c7e90822c6a3aebb23
Alessio Pannozzo <flypilot.51@gmail.com>
2025-04-28 12:27:49 +0200

    Initial commit

---
+++ db/db_setup.go
@@ -0,0 +1,82 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func SetupDatabase() {
+	// Open a connection to the SQLite database
+	db, err := sql.Open("sqlite3", "./ontrek.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+
+	// Crea tabella users
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id TEXT PRIMARY KEY, -- UUID
+		email TEXT NOT NULL UNIQUE,
+		password_hash TEXT NOT NULL,
+		name TEXT,
+		created_at TEXT NOT NULL -- ISO8601 format
+	);
+	`
+	_, err = db.Exec(createUsersTable)
+	if err != nil {
+		log.Fatalf("Errore creazione tabella users: %v", err)
+	}
+
+	// Crea tabella activities
+	createActivitiesTable := `
+	CREATE TABLE IF NOT EXISTS activities (
+		id TEXT PRIMARY KEY, -- UUID
+		user_id TEXT NOT NULL,
+		title TEXT NOT NULL,
+		description TEXT,
+		start_time TEXT,
+		end_time TEXT,
+		created_at TEXT NOT NULL,
+
+		km_percorsi FLOAT,
+		dislivello_positivo FLOAT,
+		dislivello_negativo FLOAT,
+		altezza_partenza FLOAT,
+		altezza_massima FLOAT,
+		traccia TEXT, -- JSON array di punti GPS
+
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	);
+	`
+	_, err = db.Exec(createActivitiesTable)
+	if err != nil {
+		log.Fatalf("Errore creazione tabella activities: %v", err)
+	}
+
+	// Crea tabella gpx_files
+	createGpxFilesTable := `
+	CREATE TABLE IF NOT EXISTS gpx_files (
+		id INTEGER PRIMARY KEY AUTOINCREMENT, -- INTEGER ID autoincrement
+		activity_id TEXT NOT NULL,
+		filename TEXT NOT NULL,
+		storage_path TEXT NOT NULL,
+		upload_date TEXT NOT NULL,
+		stats TEXT, -- JSON string
+		FOREIGN KEY (activity_id) REFERENCES activities(id) ON DELETE CASCADE
+	);
+	`
+	_, err = db.Exec(createGpxFilesTable)
+	if err != nil {
+		log.Fatalf("Errore creazione tabella gpx_files: %v", err)
+	}
+
+	fmt.Println("Database e tabelle create correttamente!")
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"OnTrek/api"
+	"OnTrek/db"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	// Call the setup function from dbsetup package
+	db.SetupDatabase()
+
+	router := gin.Default()
+
+	// AUTH API
+	router.POST("/auth/register", api.PostRegister) // crea un nuovo utente
+	router.POST("/auth/login")                      // ritorna access token
+
+	// GPX API
+	router.POST("/gpx/upload") // carica un file GPX
+	router.GET("/gpx/:id")     // scarica un GPX specifico
+	router.GET("/gpx/list")    // lista dei GPX caricati dall'utente
+	router.DELETE("/gpx/:id")  // elimina un GPX
+
+	// STATS API
+	router.GET("/stats/global")  // statistiche globali (tot km, tot salite, etc)
+	router.GET("/stats/gpx/:id") // statistiche di una singola traccia
+
+	// USER API
+	router.GET("/user/profile")    // dati personali
+	router.PUT("/user/profile")    // aggiorna i dati personali
+	router.DELETE("/user/account") // cancella l'account
+
+	// ACTIVITY API
+	router.GET("/activity/find") // manda la posizione e riceve l'id della traccia GPX più vicina
+
+	router.Run(":8080")
+}
