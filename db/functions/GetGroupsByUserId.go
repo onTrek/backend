98d051ee37bd04123431ef7df265ef3e387fd3ab
Alessio Pannozzo <73233717+AP71@users.noreply.github.com>
2025-06-23 19:54:00 +0200

    Update GetGroupsByUserId.go

--- db/functions/GetGroupsByUserId.go
+++ db/functions/GetGroupsByUserId.go
@@ -8,8 +8,6 @@ import (
 func GetGroupsByUserId(db *sql.DB, userId string) ([]utils.Group, error) {
 
 	var groups []utils.Group
-	var fileId *int
-	var fileName *string
 	// Get the Groups for the user
 	query := `SELECT g.id, g.description, g.created_by, g.created_at, gf.id, gf.filename FROM groups g JOIN group_members gm ON g.id = gm.group_id LEFT JOIN gpx_files gf ON g.file_id = gf.id WHERE gm.user_id = ? ORDER BY g.last_update DESC`
 	rows, err := db.Query(query, userId)
@@ -20,19 +18,22 @@ func GetGroupsByUserId(db *sql.DB, userId string) ([]utils.Group, error) {
 
 	for rows.Next() {
 		var s utils.Group
+		var fileID sql.NullInt64
+        	var fileName sql.NullString
+		
 		err := rows.Scan(&s.ID, &s.Description, &s.CreatedBy, &s.CreatedAt, &fileId, &fileName)
 
-		if fileId != nil {
-		    s.File.ID = *fileId
-		} else {
-		    s.File.ID = -1
-		}
-		
-		if fileName != nil {
-		    s.File.Filename = *fileName
-		} else {
-		    s.File.Filename = ""
-		}
+	        if fileID.Valid {
+	            s.File.ID = int(fileID.Int64)
+	        } else {
+	            s.File.ID = -1
+	        }
+	
+	        if fileName.Valid {
+	            s.File.Filename = fileName.String
+	        } else {
+	            s.File.Filename = ""
+	        }
 		
 		if err != nil {
 			return []utils.Group{}, err
