658b8432f40ec0f1618d2355d628c13ce7b95475
Alessio Pannozzo <73233717+AP71@users.noreply.github.com>
2025-07-17 14:04:21 +0200

    Update functions.go

--- utils/functions.go
+++ utils/functions.go
@@ -239,7 +239,7 @@ func CreateMap(file *multipart.FileHeader, storagePath string) error {
 
 	if HaversineDistance(start[0], start[1], end[0], end[1]) > 60 {
 		urlPath = fmt.Sprintf(
-			"https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/path-2+1a5fb4(%s),pin-s-pitch+00c924(%s),pin-s-racetrack+00c924(%s)/%f,%f,%.2f,0/%.0fx%.0f?access_token=%s",
+			"https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/path-2+1a5fb4(%s),pin-s-car+f74e4e(%s),pin-s-racetrack+00c924(%s)/%f,%f,%.2f,0/%.0fx%.0f?access_token=%s",
 			path,
 			startMarker,
 			endMarker,
@@ -252,7 +252,7 @@ func CreateMap(file *multipart.FileHeader, storagePath string) error {
 		)
 	} else {
 		urlPath = fmt.Sprintf(
-			"https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/path-2+1a5fb4(%s),pin-s-pitch+00c924(%s)/%f,%f,%.2f,0/%.0fx%.0f?access_token=%s",
+			"https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/path-2+1a5fb4(%s),pin-s-car+f74e4e(%s)/%f,%f,%.2f,0/%.0fx%.0f?access_token=%s",
 			path,
 			startMarker,
 			centerLon,
